4349ca6888c8087cca41e1ebd26fde681b63b498
cmb69 <cmbecker69@gmx.de>
2014-07-31 18:52:50 +0000

    prevented direct access and usage from unsupported CMSimple_XH versions

--- index.php
+++ index.php
@@ -14,6 +14,22 @@
  * @link      http://3-magi.net/?CMSimple_XH/Twocents_XH
  */
 
+/*
+ * Prevent direct access and usage from unsupported CMSimple_XH versions.
+ */
+if (!defined('CMSIMPLE_XH_VERSION')
+    || strpos(CMSIMPLE_XH_VERSION, 'CMSimple_XH') !== 0
+    || version_compare(CMSIMPLE_XH_VERSION, 'CMSimple_XH 1.6', 'lt')
+) {
+    header('HTTP/1.1 403 Forbidden');
+    header('Content-Type: text/plain; charset=UTF-8');
+    die(<<<EOT
+Twocents_XH detected an unsupported CMSimple_XH version.
+Uninstall Twocents_XH or upgrade to a supported CMSimple_XH version!
+EOT
+    );
+}
+
 /**
  * The data source layer.
  */
