bf86054bfbbadd2b8dbf1c28a62b72859a00b40a
cmb69 <cmbecker69@gmx.de>
2014-08-01 13:46:31 +0000

    added Twocents_Mailer

---
+++ classes/Service.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * The service layer.
+ *
+ * PHP version 5
+ *
+ * @category  CMSimple_XH
+ * @package   Twocents
+ * @author    Peter Harteg <peter@harteg.dk>
+ * @author    The CMSimple_XH developers <devs@cmsimple-xh.org>
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 1999-2009 <http://cmsimple.org/>
+ * @copyright 2009-2014 The CMSimple_XH developers <http://cmsimple-xh.org/?The_Team>
+ * @copyright 2014 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Twocents_XH
+ */
+
+/**
+ * The mailers.
+ *
+ * @category CMSimple_XH
+ * @package  Twocents
+ * @author   The CMSimple_XH developers <devs@cmsimple-xh.org>
+ * @author   Christoph M. Becker <cmbecker69@gmx.de>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @link     http://3-magi.net/?CMSimple_XH/Twocents_XH
+ */
+class Twocents_Mailer
+{
+    /**
+     * The line break characters.
+     *
+     * @var string
+     */
+    private $_lineBreak;
+
+    /**
+     * Makes and returns a new mailer.
+     *
+     * @param string $lineBreak A line break string.
+     *
+     * @return Twocents_Mailer
+     */
+    public static function make($lineBreak = "\r\n")
+    {
+        return new self($lineBreak);
+    }
+
+    /**
+     * Initializes a new instance.
+     *
+     * @param string $lineBreak A line break string.
+     *
+     * @return void
+     */
+    private function __construct($lineBreak)
+    {
+        $this->_lineBreak = (string) $lineBreak;
+    }
+
+    /**
+     * Returns whether an email address is valid.
+     *
+     * For simplicity we are not aiming for full compliance with RFC 5322.
+     * The local-part must be a dot-atom-text. The domain is checked with
+     * gethostbyname() after applying idn_to_ascii(), if the latter is available.
+     *
+     * @param string $address An email address.
+     *
+     * @return bool
+     */
+    public function isValidAddress($address)
+    {
+        $atext = '[!#-\'*+\-\/-9=?A-Z^-~]';
+        $dotAtomText = $atext . '(?:' . $atext . '|\.)*';
+        $pattern = '/^(' . $dotAtomText . ')@([^@]+)$/u';
+        if (!preg_match($pattern, $address, $matches)) {
+            return false;
+        }
+        $local = $matches[1];
+        $domain = $matches[2];
+        if (function_exists('idn_to_ascii')) {
+            $domain = defined('INTL_IDNA_VARIANT_UTS46')
+                ? idn_to_ascii($domain, 0, INTL_IDNA_VARIANT_UTS46)
+                : idn_to_ascii($domain);
+        }
+        if (gethostbyname($domain) == $domain) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Sends a UTF-8 encoded mail.
+     *
+     * @param string $to                Receiver, or receivers of the mail.
+     * @param string $subject           Subject of the email to be sent.
+     * @param string $message           Message to be sent.
+     * @param string $additionalHeaders String to be inserted at the end of the
+     *                                  email header.
+     *
+     * @return bool Whether the mail was accepted for delivery.
+     */
+    public function send($to, $subject, $message, $additionalHeaders = '')
+    {
+        $header = 'MIME-Version: 1.0' . $this->_lineBreak
+            . 'Content-Type: text/plain; charset=UTF-8; format=flowed'
+            . $this->_lineBreak
+            . 'Content-Transfer-Encoding: base64' . $this->_lineBreak
+            . $additionalHeaders;
+        $subject = $this->_encodeMIMEFieldBody($subject);
+        $message = preg_replace(
+            '/(?:\r\n|\r|\n)/', $this->_lineBreak, trim($message)
+        );
+        $message = chunk_split(base64_encode($message));
+        return mail($to, $subject, $message, $header);
+    }
+
+    /**
+     * Returns the body of an email header field as "encoded word" (RFC 2047)
+     * with "folding" (RFC 5322), if necessary.
+     *
+     * @param string $text The body of the MIME field.
+     *
+     * @return string
+     *
+     * @todo Don't we have to fold overlong pure ASCII texts also?
+     */
+    private function _encodeMIMEFieldBody($text)
+    {
+        if (!preg_match('/(?:[^\x00-\x7F])/', $text)) { // ASCII only
+            return $text;
+        } else {
+            $lines = array();
+            do {
+                $i = 45;
+                if (strlen($text) > $i) {
+                    while ((ord($text[$i]) & 0xc0) == 0x80) {
+                        $i--;
+                    }
+                    $lines[] = substr($text, 0, $i);
+                    $text = substr($text, $i);
+                } else {
+                    $lines[] = $text;
+                    $text = '';
+                }
+            } while ($text != '');
+            $func = create_function(
+                '$l', 'return \'=?UTF-8?B?\' . base64_encode($l) . \'?=\';'
+            );
+            return implode($this->_lineBreak . ' ', array_map($func, $lines));
+        }
+    }
+}
+
+?>
---
+++ tests/unit/MailerTest.php
@@ -0,0 +1,154 @@
+<?php
+
+/**
+ * Testing the mailer.
+ *
+ * PHP version 5
+ *
+ * @category  Testing
+ * @package   Twocents
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2014 Christoph M. Becker <http://3-magi.net>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Twocents_XH
+ */
+
+require_once './vendor/autoload.php';
+require_once './classes/Service.php';
+
+/**
+ * Testing the mailer.
+ *
+ * @category Testing
+ * @package  Twocents
+ * @author   Christoph M. Becker <cmbecker69@gmx.de>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @link     http://3-magi.net/?CMSimple_XH/Twocents_XH
+ */
+class MailerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * The test subject.
+     *
+     * @var Twocents_Mailer
+     */
+    private $_subject;
+
+    /**
+     * The gethostbyname() mock.
+     *
+     * @var object
+     */
+    private $_gethostbynameMock;
+
+    /**
+     * The mail() mock.
+     *
+     * @var object
+     */
+    private $_mailMock;
+
+    /**
+     * Sets up the test fixture.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->_subject = Twocents_Mailer::make();
+        $this->_gethostbynameMock = new PHPUnit_Extensions_MockFunction(
+            'gethostbyname', $this->_subject
+        );
+        $this->_mailMock = new PHPUnit_Extensions_MockFunction(
+            'mail', $this->_subject
+        );
+    }
+
+    /**
+     * Tests a valid address.
+     *
+     * @return void
+     */
+    public function testValidAddress()
+    {
+        $this->_gethostbynameMock->expects($this->any())->will(
+            $this->returnValue('127.0.0.1')
+        );
+        $this->assertTrue($this->_subject->isValidAddress('cmbecker69@gmx.de'));
+    }
+
+    /**
+     * Tests the a local part with a space is an invalid address.
+     *
+     * @return void
+     */
+    public function testLocalPartWithSpaceIsInvalidAddress()
+    {
+        $this->assertFalse($this->_subject->isValidAddress('c b@example.com'));
+    }
+
+    /**
+     * Tests that a not existing domain is an invalid address.
+     *
+     * @return void
+     */
+    public function testNotExistingDomainIsInvalidAddress()
+    {
+        $this->_gethostbynameMock->expects($this->any())->will(
+            $this->returnValue('test.invalid')
+        );
+        $this->assertFalse($this->_subject->isValidAddress('me@test.invalid'));
+    }
+
+    /**
+     * Tests that sending an ASCII subject calls mail with correct arguments.
+     *
+     * @return void
+     */
+    public function testSendAsciiSubjectCallsMailWithCorrectArguments()
+    {
+        $this->_mailMock->expects($this->once())->with(
+            'cmbecker69@gmx',
+            'A test',
+            "TG9yZW0gaXBzdW0=\r\n",
+            "MIME-Version: 1.0\r\n"
+            . "Content-Type: text/plain; charset=UTF-8; format=flowed\r\n"
+            . "Content-Transfer-Encoding: base64\r\n"
+            . "From: cmbecker69@gmx.de"
+        );
+        $this->_subject->send(
+            'cmbecker69@gmx',
+            'A test',
+            'Lorem ipsum',
+            'From: cmbecker69@gmx.de'
+        );
+    }
+
+    /**
+     * Tests that sending an UTF-8 subject calls mail with correct arguments.
+     *
+     * @return void
+     */
+    public function testSendUtf8SubjectCallsMailWithCorrectArguments()
+    {
+        $this->_mailMock->expects($this->once())->with(
+            'cmbecker69@gmx',
+            '=?UTF-8?B?RHJpdmluZyB5b3VyIEJNVyBkb3duIHRoZSByb2FkLCBpcyBGYWhydmVy'
+            . "Z24=?=\r\n =?UTF-8?B?w7xnZW4=?=",
+            "TG9yZW0gaXBzdW0=\r\n",
+            "MIME-Version: 1.0\r\n"
+            . "Content-Type: text/plain; charset=UTF-8; format=flowed\r\n"
+            . "Content-Transfer-Encoding: base64\r\n"
+            . "From: cmbecker69@gmx.de"
+        );
+        $this->_subject->send(
+            'cmbecker69@gmx',
+            "Driving your BMW down the road, is Fahrvergn\xC3\xBCgen",
+            'Lorem ipsum',
+            'From: cmbecker69@gmx.de'
+        );
+    }
+}
+
+?>
