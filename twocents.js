f6bd394a5a446352a90808d26fef542a01a0fbcc
Christoph M. Becker <cmbecker69@gmx.de>
2017-02-19 23:26:50 +0100

    Use GNU style license header

--- twocents.js
+++ twocents.js
@@ -1,8 +1,20 @@
-/**
- * Twocents_XH
+/*!
+ * Copyright 2014-2017 Christoph M. Becker
  *
- * @author  Christoph M. Becker <cmbecker69@gmx.de>
- * @license GPL-3.0+
+ * This file is part of Twocents_XH.
+ *
+ * Twocents_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Twocents_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Twocents_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*jslint browser: true, maxlen: 80 */
