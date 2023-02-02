e70367002a457ebb435d729b873b8de296d992b0
Christoph M. Becker <cmbecker69@gmx.de>
2023-02-02 15:40:00 +0100

    Simplify direct access prevention

--- index.php
+++ index.php
@@ -19,20 +19,9 @@
  * along with Twocents_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Prevent direct access and usage from unsupported CMSimple_XH versions.
- */
-if (!defined('CMSIMPLE_XH_VERSION')
-    || strpos(CMSIMPLE_XH_VERSION, 'CMSimple_XH') !== 0
-    || version_compare(CMSIMPLE_XH_VERSION, 'CMSimple_XH 1.6', 'lt')
-) {
-    header('HTTP/1.1 403 Forbidden');
-    header('Content-Type: text/plain; charset=UTF-8');
-    die(<<<EOT
-Twocents_XH detected an unsupported CMSimple_XH version.
-Uninstall Twocents_XH or upgrade to a supported CMSimple_XH version!
-EOT
-    );
+if (!defined('CMSIMPLE_XH_VERSION')) {
+    header("HTTP/1.1 403 Forbidden");
+    exit;
 }
 
 /**
