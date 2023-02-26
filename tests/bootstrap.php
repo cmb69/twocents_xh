bb442d131948d0284fe68892bfb071aeb258cc22
Christoph M. Becker <cmbecker69@gmx.de>
2023-02-26 12:14:45 +0100

    Use autoloading for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -25,11 +25,19 @@ require_once '../../cmsimple/functions.php';
 require_once '../../cmsimple/adminfuncs.php';
 include_once '../../cmsimple/utf8.php';
 require_once '../../cmsimple/classes/CSRFProtection.php';
-require_once './classes/Comment.php';
-require_once './classes/Db.php';
-require_once './classes/HtmlString.php';
-require_once './classes/MainAdminController.php';
-require_once './classes/Pagination.php';
-require_once './classes/SpamFilter.php';
-require_once './classes/Topic.php';
-require_once './classes/View.php';
+
+spl_autoload_register(function (string $className) {
+    $parts = explode("\\", $className);
+    if ($parts[0] !== "Twocents") {
+        return;
+    }
+    if (count($parts) === 3) {
+        $parts[1] = strtolower($parts[1]);
+    }
+    $filename = implode("/", array_slice($parts, 1));
+    if (is_readable("./classes/$filename.php")) {
+        include_once "./classes/$filename.php";
+    } elseif (is_readable("./tests/$filename.php")) {
+        include_once "./tests/$filename.php";
+    }
+});
